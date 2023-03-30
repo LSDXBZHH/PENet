6decb1c3d1158d8ca886866cf6897d4e22dfcf9c
LSDXBZHH <787637446@qq.com>
2023-03-30 22:07:20 +0800

    tools

---
+++ tools.py
@@ -0,0 +1,45 @@
+import numpy as np
+import torch.utils.data as util_data
+from torchvision import transforms
+import torch
+from PIL import Image
+from tqdm import tqdm
+import torchvision.datasets as dsets
+
+
+
+def compute_result(dataloader, net, device):
+    bs, clses = [], []
+    net.eval()
+    for img, cls, _ in tqdm(dataloader):
+        clses.append(cls)
+        bs.append((net(img.to(device))).data.cpu())
+    return torch.cat(bs).sign(), torch.cat(clses)
+
+
+def CalcHammingDist(B1, B2):
+    q = B2.shape[1]
+    distH = 0.5 * (q - np.dot(B1, B2.transpose()))
+    return distH
+
+
+def CalcTopMap(rB, qB, retrievalL, queryL, topk):
+    num_query = queryL.shape[0]
+    topkmap = 0
+    for iter in tqdm(range(num_query)):
+        gnd = (np.dot(queryL[iter, :], retrievalL.transpose()) > 0).astype(np.float32)
+        hamm = CalcHammingDist(qB[iter, :], rB)
+        ind = np.argsort(hamm)
+        gnd = gnd[ind]
+
+        tgnd = gnd[0:topk]
+        tsum = np.sum(tgnd).astype(int)
+        if tsum == 0:
+            continue
+        count = np.linspace(1, tsum, tsum)
+
+        tindex = np.asarray(np.where(tgnd == 1)) + 1.0
+        topkmap_ = np.mean(count / (tindex))
+        topkmap = topkmap + topkmap_
+    topkmap = topkmap / num_query
+    return topkmap
